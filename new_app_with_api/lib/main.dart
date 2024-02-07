41ce37680bb676455ca72d40915808756427e74d
Royhan <royhankeren12@gmail.com>
2024-02-07 15:23:15 +0700

    article models

---
+++ new_app_with_api/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainActivity());
+}
+
+class MainActivity extends StatelessWidget {
+  const MainActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
