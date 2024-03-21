9551a2fb5579d659200f1232b8f4cb5c94a01b14
Royhan <royhankeren12@gmail.com>
2024-03-21 13:11:43 +0700

    counter_provider dart

---
+++ LatihanRiverpod/lib/providers/counter_provider.dart
@@ -0,0 +1,9 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'counter_provider.g.dart';
+
+@riverpod
+class CounterNotifier extends _$CounterNotifier {
+  @override
+  int build() => 0;
+}
