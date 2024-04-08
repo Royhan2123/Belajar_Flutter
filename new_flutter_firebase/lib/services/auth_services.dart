f2cdb8d5d647f746599feee6f09cff527fe1d504
Royhan <royhankeren12@gmail.com>
2024-04-08 23:58:12 +0700

    loginServices

--- new_flutter_firebase/lib/services/auth_services.dart
+++ new_flutter_firebase/lib/services/auth_services.dart
@@ -1,2 +1,48 @@
-String baseUrl = "https://bwabank.my.id/api";
-  
+import 'package:dio/dio.dart';
+import 'package:new_flutter_firebase/models/login_models.dart';
+import 'package:new_flutter_firebase/models/users_models.dart';
+
+class AuthServices {
+  String baseUrl = "https://bwabank.my.id/api";
+  final dio = Dio();
+
+  Future<bool> checkEmail(String email) async {
+    try {
+      final response = await dio.post(
+        "$baseUrl/is-email-exist",
+        data: {
+          "email": email,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        return response.data["is-email-exist"];
+      } else {
+        return response.data["errors"];
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<UserModels> signIn(LoginModels data) async {
+    try {
+      final response = await dio.post(
+        "$baseUrl/login",
+        data: {
+          data.toJson(),
+        },
+      );
+
+      if (response.statusCode == 200) {
+        UserModels user = UserModels.fromJson(response.data);
+        user.copyWith(password: data.password);
+        return user;
+      } else {
+        throw response.data["message"];
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
